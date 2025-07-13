27fd89b2c9321ed667145056abe762e8351c2a83
KidusHawoltu <kidushawoltu@gmail.com>
2025-07-13 18:52:34 +0300

    Debug Word frequency count

--- Task 2/Task2.go
+++ Task 2/Task2.go
@@ -8,7 +8,7 @@ func WordFrequencyCount(str string) map[string]int {
 	i, j, ans := 0, 0, make(map[string]int)
 	for j < len(str) {
 		// Assuming Numbers are part of words
-		for ; j < len(str) && ((65 <= str[j] && str[j] <= 90) || (97 <= str[j] && str[j] <= 122) || (48 <= str[j] && str[j] <= 58)); j++ {
+		for ; j < len(str) && ((65 <= str[j] && str[j] <= 90) || (97 <= str[j] && str[j] <= 122) || (48 <= str[j] && str[j] < 58)); j++ {
 		}
 		if i != j {
 			ans[strings.ToLower(str[i:j])]++
